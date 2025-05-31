4aacea9bfcb0e93a3647b7ea8ae0a481909901cf
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2025-05-31 23:31:03 +0100

    feat:replace exploreshopimage

--- src/components/home/shop.tsx
+++ src/components/home/shop.tsx
@@ -6,7 +6,7 @@ export const Shop = () => {
     <section className="hidden md:block w-full bg-blue-primary h-[400px]">
       <div className="max-w-screen-xl grid w-full mx-auto h-full p-10 gap-10 grid-cols-1 md:grid-cols-2">
         <img
-          src="/assets/shop.jpg"
+          src="https://i.guim.co.uk/img/media/bb5e732bdd0ae3bafca2ca7182232a562a824dfa/0_331_6541_3924/master/6541.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=2d7e945e63c81df2a9e3fdbf154c7958"
           alt="A shopper browsing products in a supermarket"
           className="h-3/4 object-cover w-full border-white border-2 rounded-3xl"
         />
