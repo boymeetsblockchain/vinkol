5f8d1f9bcb771b5066a864afee1dce5c1bfc9dcc
unknown <iykee0333@gmail.com>
2025-12-03 00:02:51 +0100

    animate stats

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -6,9 +6,10 @@ import { FaUsers } from "react-icons/fa";
 import { LuPackageCheck } from "react-icons/lu";
 import { RiMotorbikeFill } from "react-icons/ri";
 import { Skeleton } from "../ui/skeleton";
+import CountUp from "./countup";
 
 const Stats = () => {
-  const { data, isLoading } = useGetStats();
+  // const { data, isLoading } = useGetStats();
 
   return (
     <section className="max-w-screen-2xl w-full p-10 md:p-20 mx-auto min-h-[400px]">
@@ -17,41 +18,33 @@ const Stats = () => {
       </h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {isLoading ? (
-          <>
-            <Skeleton className="w-full h-[300px]" />
-            <Skeleton className="w-full h-[300px]" />
-            <Skeleton className="w-full h-[300px]" />
-          </>
-        ) : (
-          <>
-            <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
-              <RiMotorbikeFill size={48} className="text-blue-600" />
-              <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
-                {data?.data?.verifiedRiders}
-              </h1>
-              <p className="text-xl text-center font-medium">Verified Riders</p>
-            </div>
-            <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
-              <FaUsers size={48} className="text-blue-600" />
-              <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
-                {data?.data?.realUsers}
-              </h1>
-              <p className="text-xl text-center font-medium">
-                Registered Users
-              </p>
-            </div>
-            <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
-              <LuPackageCheck size={48} className="text-blue-600" />
-              <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
-                {data?.data?.deliveredOrders}
-              </h1>
-              <p className="text-xl text-center font-medium">
-                Delivered Orders
-              </p>
-            </div>
-          </>
-        )}
+        <>
+          <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
+            <RiMotorbikeFill size={48} className="text-blue-600" />
+            <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
+              {/* {data?.data?.verifiedRiders} */}
+              <CountUp endValue={200} suffix="+" />
+            </h1>
+            <p className="text-xl text-center font-medium">Verified Riders</p>
+          </div>
+          <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
+            <LuPackageCheck size={48} className="text-blue-600" />
+            <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
+              {/* {data?.data?.realUsers} */}
+              <CountUp endValue={500} suffix="+" />
+            </h1>
+            <p className="text-xl text-center font-medium">Orders</p>
+          </div>
+          <div className="flex items-center *: gap-y-4 flex-col bg-blue-50/30 p-8 rounded-xl">
+            {" "}
+            <FaUsers size={48} className="text-blue-600" />
+            <h1 className="font-bold text-primary text-4xl md:text-6xl text-center">
+              {/* {data?.data?.deliveredOrders} */}{" "}
+              <CountUp endValue={100} suffix="+" />
+            </h1>
+            <p className="text-xl text-center font-medium">Personal Shoppers</p>
+          </div>
+        </>
       </div>
     </section>
   );
