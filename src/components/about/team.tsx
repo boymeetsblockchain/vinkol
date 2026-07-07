7f5d215564ada18cf2ae7d94454406d7442049e1
kingstech <kingstech0001@gmail.com>
2026-07-07 21:25:43 +0100

    Made changes to the about bio

--- src/components/about/team.tsx
+++ src/components/about/team.tsx
@@ -5,19 +5,19 @@ const teamsArray = [
     name: "Emmanuel Ediale",
     position: "Founder & CEO",
     img: "/assets/coo.jpg",
-    bio: "Emmanuel is a finance professional with over 9 years of experience spanning across the Banking, Insurance, Media, and Construction industries. He holds an MSc in International Business from the University of Sunderland, UK, and is a Certified International Trade Professional (CITP).\n\nWith a unique blend of practical experience and academic foundation, Emmanuel provides strategic financial insights and plays a key role in driving daily operational excellence.",
+    bio: "Emmanuel is an accomplished finance professional with over nine years of experience across diverse industries, including Banking, Insurance, Media, and Construction. He holds a master’s degree in International Business from the University of Sunderland, UK, a postgraduate diploma with honors in Business Management from Algonquin College, Canada, and a bachelor’s degree in Insurance. Emmanuel is also a Certified International Trade Professional (CITP) in Canada. In addition to his academic qualifications, he is an Associate of the Chartered Insurance Institute of Nigeria and a Chartered Financial Forecasting, Budgeting, and Modelling Professional (CFBM). He is also a Certified Management Consultant, among several other industry-recognized certifications. Leveraging his broad industry expertise and solid academic foundation, Emmanuel provides strategic financial insights and plays a key role in driving daily operational excellence.",
   },
   {
     name: "Francis Ediale",
     position: "Executive Director",
     img: "/assets/ed.jpg",
-    bio: "Francis is a highly skilled Operations Manager with over 10 years of experience in maintaining organizational quality and safety standards. He holds a bachelor’s degree in Petroleum Engineering from the University of Benin, Nigeria and a Postgraduate Diploma in International Business Management from Seneca College, Canada.\n\nWith a strong background in leadership, process management, human resource management, and project management, Francis is adept at driving efficiency and achieving organizational objectives. He brings exceptional communication, interpersonal, and conflict resolution skills, consistently exceeding performance expectations and fostering a collaborative work environment.",
+    bio: "Francis is a highly skilled operations manager with over 10 years of experience in maintaining organizational quality and safety standards. He holds a Bachelor’s degree in petroleum engineering from the University of Benin, Nigeria, and a Postgraduate diploma in International Business Management from Seneca College, Canada. With a strong leadership, human resources, and project management background, Francis is adept at driving efficiency and achieving organizational objectives. He brings exceptional communication, interpersonal, and conflict resolution skills, consistently exceeding performance expectations and fostering a collaborative work environment.",
   },
   {
     name: "Vincent Ediale",
     position: "Chairman",
     img: "/assets/daddy.jpg",
-    bio: "Vincent is a seasoned professional with over two decades of experience in the Oil & Gas, Construction, and International Trade business. A graduate of the prestigious University of Ibadan, he began his career upon graduation in the 1980’s with Julius Berger and later on contributed to the growth of Zenon Oil and Gas in Victoria Island, Lagos, Nigeria.\n\nThroughout the course of his career, Vincent has demonstrated a proven ability to build and scale successful businesses, leveraging his expertise to deliver sustainable growth and operational excellence.",
+    bio: "Vincent is a seasoned professional with over two decades of experience in the Oil & Gas, Construction, and International Trade sectors. A graduate of the prestigious University of Ibadan, he began his career in the 1980s with Julius Berger and later contributed to the growth of Zenon Oil and Gas in Victoria Island, Lagos, Nigeria. Throughout his career, Vincent has demonstrated a proven ability to build and scale successful businesses, leveraging his expertise to deliver sustainable growth and operational excellence.",
   },
 ];
 
